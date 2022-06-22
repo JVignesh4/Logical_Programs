32bbf55f96ed040f5f1df82c70b97318ba0b093c
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-22 09:28:58 +0530

    Added: To Find Prime Number

--- src/com/logicalprograms/Main.java
+++ src/com/logicalprograms/Main.java
@@ -1,9 +1,22 @@
 package com.logicalprograms;
 
+import java.util.Scanner;
+
 import static com.logicalprograms.LogicalPrograms.fibonacciSeries;
+import static com.logicalprograms.LogicalPrograms.primeNumber;
 
 public class Main {
     public static void main(String[] args) {
-        fibonacciSeries();
+        Scanner input = new Scanner(System.in);
+        System.out.println("1.Fibonacci Series");
+        System.out.println("2.Prime Number");
+        System.out.println("Enter the choice: ");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                fibonacciSeries();
+            case 2:
+                primeNumber();
+        }
     }
 }
