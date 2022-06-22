875efde990bb55e8c52fd0a02aa70192a7eb353f
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-22 08:48:51 +0530

    Added: Logical Programs

---
+++ src/com/logicalprograms/LogicalPrograms.java
@@ -0,0 +1,7 @@
+package com.logicalprograms;
+
+import java.util.Scanner;
+
+public class LogicalPrograms {
+
+}
---
+++ src/com/logicalprograms/Main.java
@@ -0,0 +1,7 @@
+package com.logicalprograms;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
