c3852ac7cc7eb5e3a22373661c3d80fd8888be5c
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-23 08:17:09 +0530

    Added: Coupon Number Program

--- src/com/logicalprograms/Main.java
+++ src/com/logicalprograms/Main.java
@@ -9,6 +9,9 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("1.Fibonacci Series");
         System.out.println("2.Prime Number");
+        System.out.println("3.Perfect Number");
+        System.out.println("4.Reverse Number");
+        System.out.println("5.Coupon Number ");
         System.out.println("Enter the choice: ");
         int choice = input.nextInt();
         switch (choice) {
@@ -20,6 +23,8 @@ public class Main {
                 perfectNumber();
             case 4:
                 reverseNumber();
+            case 5:
+                couponNumber(5);
         }
     }
 }
